0e6055b1e8baac2cf2355ad94c44e692b64a7a6c
rpmonteiro <ric.p.monteiro@gmail.com>
2016-03-30 17:16:14 +0100

    map to how it was before

--- public/app.js
+++ public/app.js
@@ -1,7 +1,45 @@
 function initialize() {
-  var zoom = 14;
-  var coordinates = {lat: 55.951998, lng: -3.18997};
-  var map = new Map(coordinates, zoom);
+  var styles = [{
+    'stylers': [{
+      'visibility': 'off'
+    }]
+    }, {
+    'featureType': 'landscape',
+    'stylers': [{
+      'visibility': 'on'
+    }, {
+      'color': '#f3f4f4'
+    }]
+    }, {
+    'featureType': 'water',
+    'stylers': [{
+      'visibility': 'on'
+    }, {
+      'color': '#81D4FA'
+    }]
+    }, {
+    'elementType': 'labels',
+    'stylers': [{
+      'visibility': 'off'
+    }]
+    }, {
+    'featureType': 'landscape.man_made',
+    'elementType': 'geometry',
+    'stylers': [{
+      'weight': 0.9
+    }, {
+      'visibility': 'off'
+    }]
+  }];
+
+  var mapOptions = {
+    disableDefaultUI: true,
+    styles: styles
+  };
+
+  var initialZoom = 3;
+  var initialMapCoordinates = {lat: 38.470794, lng: 6.679688};
+  var map = new Map(initialMapCoordinates, initialZoom, mapOptions);
 }
 
 window.onload = initialize;
--- public/map.js
+++ public/map.js
@@ -1,26 +1,12 @@
-function Map(latlng, zoom) {
+function Map(latlng, zoom, options) {
   var mapDiv = document.getElementById('map');
 
   this.googleMap = new google.maps.Map(mapDiv, {
     center: latlng,
-    zoom: zoom
+    zoom: zoom,
+    options: options
   });
 
-  this.addMarker = function(coordinates, label, title) {
-    var marker = new google.maps.Marker({
-      position: coordinates,
-      map: this.googleMap,
-      label: label,
-      title: title
-    });
-    return marker;
-  }
-
-  this.bindClick = function() {
-    google.maps.event.addListener(this.googleMap, 'click', function(event) {
-      coordinates = {lat: event.latLng.lat(), lng: event.latLng.lng()};
-      this.addMarker(coordinates);
-    }.bind(this));
-  }
+  this.googleMap.setMapTypeId(google.maps.MapTypeId.SATELLITE);
 
 }
